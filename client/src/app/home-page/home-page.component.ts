650f3d020d3f910e57ea4a381d4782dcf939d825
meweryk <39477539+meweryk@users.noreply.github.com>
2020-03-25 22:41:15 +0200

    add home page

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -10,10 +10,12 @@ export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('parallax1') parallax1Ref: ElementRef
   @ViewChild('parallax2') parallax2Ref: ElementRef
   @ViewChild('parallax3') parallax3Ref: ElementRef
+  @ViewChild('parallax4') parallax4Ref: ElementRef
 
   parallax1: MaterialInstance
   parallax2: MaterialInstance
   parallax3: MaterialInstance
+  parallax4: MaterialInstance
 
   constructor() { }
 
@@ -24,12 +26,14 @@ export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
     this.parallax1 = MaterialService.initParallax(this.parallax1Ref)
     this.parallax2 = MaterialService.initParallax(this.parallax2Ref)
     this.parallax3 = MaterialService.initParallax(this.parallax3Ref)
+    this.parallax4 = MaterialService.initParallax(this.parallax4Ref)
   }
 
   ngOnDestroy() {
     this.parallax1.destroy()
     this.parallax2.destroy()
     this.parallax3.destroy()
+    this.parallax4.destroy()
   }
 
 }
